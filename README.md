acdd602ae630b3e977c2563fdf3a8e8deb93bf13
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-11-21 03:27:42 +0100

    Docs: updatedREADME

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning Web5 🚀
+
+Web5 is actually a technology I love, I love the fact that it is a technology that offers privacy and decentralization without the use of blockchain, how awesome is that, though I'm still learning about it, but I really love how it operates and the stuff I can build with it, also when **blackgirlbytes** talked about it and created a burn book, it was just soo cool. I can't wait to learn and build awesome stuff.
---
+++ index.js
@@ -0,0 +1,24 @@
+import { Web5 } from "@web5/api";
+import { webcrypto } from "node:crypto";
+import { read } from "node:fs";
+
+if (!globalThis.crypto) globalThis.crypto = webcrypto;
+
+const { web5, did: aliceDid } = await Web5.connect();
+
+const { record } = await web5.dwn.records.create({
+  data: "I love you jesus!",
+  message: {
+    dataFormat: "text/plain",
+  },
+});
+
+const readResult = await record.data.text();
+
+const updateResult = await record.update({
+  data: "I love you jesus again!",
+});
+
+console.log(await updateResult);
+console.log(readResult);
+console.log(await record.data.text());
